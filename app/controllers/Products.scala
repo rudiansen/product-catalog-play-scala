d49578633134cddd4c978b7a555e8cab7c78bc42
Rudiansen Gunawan <rudiansen.gunawan@id.abb.com>
2018-12-06 17:24:36 +0800

    initial commit

---
+++ app/controllers/Products.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import models.Product
+
+class Products @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def list = Action { implicit request =>
+    val products = Product.findAll
+    Ok(views.html.products.list(products))
+  }
+
+  def show(ean: Long) = Action {implicit request =>
+
+    Product.findByEan(ean).map { product =>
+      Ok(views.html.products.details(product))
+    }.getOrElse(NotFound)
+  }
+
+}
